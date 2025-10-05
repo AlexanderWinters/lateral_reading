ddd5aea7d88d501e84c4aad36ff63aeab84f0aed
alex <alex@kknds.com>
2025-10-05 15:53:39 +0200

    added better; fixed notes

---
+++ notes.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eba2a07e6b1c233fbf2c2becdc353fe5e8a802237645ef978fe2a7b30e588544
+size 3334
--- translation.js
+++ translation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d6985988812f5165a0e4d87757d5fa4fdf79454c5749b1603bcb6716c102f9d
-size 33987
+oid sha256:ba9f4f0e89118faad91afeb1317410a3e99959e05cb3a8bcf45c2632b418c27e
+size 35062
